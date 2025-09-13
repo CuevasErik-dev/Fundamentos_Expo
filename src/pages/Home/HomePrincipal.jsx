7ed2d5bf35b96fa91fe679e4d9f69057866931ff
CuevasErik-dev <cuevas.erik.israel@gmail.com>
2025-09-13 12:40:50 -0600

    task 10 Componente Home Principal

---
+++ src/pages/Home/HomePrincipal.jsx
@@ -0,0 +1,34 @@
+import { Image, StyleSheet, Text, View, Platform, StatusBar, ScrollView } from "react-native";
+import CardBasico from "../../components/CardBasico";
+import Cardimg from "../../components/Cardimg";
+import CardV2 from "../../components/CardV2";
+import CardV3 from "../../components/CardV3";
+
+export default function HomePrincipal() {
+    return (
+        <ScrollView>
+            <View style={style.mainS}>
+                <Text style={style.title}>Pagina Principal</Text>
+                <CardBasico />
+                <Cardimg />
+                <CardV2 />
+                <CardV3 />
+            </View>
+        </ScrollView>
+    );
+}
+
+const style = StyleSheet.create({
+    mainS: {
+        flex: 1,
+        backgroundColor: "#e9c0c0ff",
+        paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 44,
+        padding: 16,
+    },
+    title: {
+        fontSize: 22,
+        fontWeight: 'bold',
+        marginTop: 20,
+        textAlign: 'center',
+    },
+});
