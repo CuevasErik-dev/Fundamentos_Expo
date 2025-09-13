b15e52ebfedd039019e0330907aec9b15b0b0328
CuevasErik-dev <cuevas.erik.israel@gmail.com>
2025-09-13 13:49:51 -0600

    task 16 Creando AppBar Basico

--- src/Lista_Alumnos/ListaAlumnos.jsx
+++ src/Lista_Alumnos/ListaAlumnos.jsx
@@ -1,9 +1,18 @@
-import { SafeAreaView,Text, StyleSheet, Platform, StatusBar } from "react-native"
+import { SafeAreaView,Text, StyleSheet, Platform, StatusBar, View } from "react-native"
+import Ionicons from '@expo/vector-icons/Ionicons'
 
 export default function App() {
     return (
         <SafeAreaView style={style.mainS}>
-            <Text>Hola</Text>
+            {/* Inicio de AppbBar */}
+            <View style={style.appBar}>
+                <Ionicons name="arrow-back-circle-outline" size={35} color='#0a0a0aff' />
+                <Text style={style.appBarTitle}>Lista de Usuarios</Text>
+                <View style={style.Barra}>
+
+                </View>
+            </View>
+            {/* Cierre de Appbar */}
         </SafeAreaView>
     );
 }
@@ -11,8 +20,25 @@ export default function App() {
 const style = StyleSheet.create({
     mainS: {
         flex: 1,
-        backgroundColor: "#e9c0c0ff",
+        backgroundColor: "#ffffffff",
         paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 44,
-        padding: 16,
     },
+    appBar:{
+        height:50,
+        width:'100%',
+        backgroundColor:'#e9c0c0ff',
+        flexDirection:'row',
+        alignItems:'center',
+        justifyContent:'space-between',
+        paddingHorizontal:16,
+        borderWidth: 0.2,
+        borderColor:'#fd8282ff' 
+    },
+    appBarTitle:{
+        fontWeight:'bold',
+        fontSize:16
+    },
+    barra:{
+        width:20,
+    }
 });
